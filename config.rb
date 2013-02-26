9a9f9476143fb18d082903c4292cf118128cb573
Benjamin Charity <ben@benjamincharity.com>
2013-02-26 11:23:17 -0500

    re-exported the svg, still using the png

--- config.rb
+++ config.rb
@@ -53,17 +53,17 @@ set :js_dir, 'js'
 
 set :images_dir, 'i'
 
-  # Activate sync extension
-  activate :sync do |sync|
-    sync.fog_provider = 'AWS' # Your storage provider
-    sync.fog_directory = 'new.benjamincharity.com' # Your bucket name
-    sync.fog_region = 'us-east-1' # The region your storage bucket is in (eg us-east-1, us-west-1, eu-west-1, ap-southeast-1 )
-    sync.aws_access_key_id = 'AKIAIBYD74YWP3MT4WOQ' # Your Amazon S3 access key
-    sync.aws_secret_access_key = 'ahIMHc6w16sF8fY0aN9sg26PRIJeA7UnK7M0iGoY' # Your Amazon S3 access secret
-    sync.existing_remote_files = 'keep' # What to do with your existing remote files? ( keep or delete )
-    # sync.gzip_compression = false # Automatically replace files with their equivalent gzip compressed version
-    # sync.after_build = false # Disable sync to run after Middleman build ( defaults to true )
-  end
+# Activate sync extension
+activate :sync do |sync|
+  sync.fog_provider = 'AWS' # Your storage provider
+  sync.fog_directory = 'new.benjamincharity.com' # Your bucket name
+  sync.fog_region = 'us-east-1' # The region your storage bucket is in (eg us-east-1, us-west-1, eu-west-1, ap-southeast-1 )
+  sync.aws_access_key_id = 'AKIAIBYD74YWP3MT4WOQ' # Your Amazon S3 access key
+  sync.aws_secret_access_key = 'ahIMHc6w16sF8fY0aN9sg26PRIJeA7UnK7M0iGoY' # Your Amazon S3 access secret
+  sync.existing_remote_files = 'keep' # What to do with your existing remote files? ( keep or delete )
+  # sync.gzip_compression = false # Automatically replace files with their equivalent gzip compressed version
+  # sync.after_build = false # Disable sync to run after Middleman build ( defaults to true )
+end
 
 # Build-specific configuration
 configure :build do
