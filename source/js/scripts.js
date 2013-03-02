3457afe49eb0cd058b55fbd2ee7f5baa0f7699dc
Benjamin Charity <ben@benjamincharity.com>
2013-03-02 14:19:20 -0500

    final tweaks

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,5 +1,113 @@
 $(function() {
 
+  var images = [
+    '1.gif',
+    '2.gif',
+    '3.gif',
+    '4.gif',
+    '5.gif',
+    '6.gif',
+    '7.gif',
+    '8.gif',
+    '9.gif',
+    '10.gif',
+    '11.gif',
+    '12.gif',
+    '13.gif',
+    '14.gif',
+    '15.gif',
+    '16.gif',
+    '17.gif',
+    '18.gif',
+    //'19.gif',
+    '20.gif',
+    '21.gif',
+    '22.gif',
+    '23.gif',
+    '24.gif',
+    '25.gif',
+    '26.gif'
+  ];
+
+  var mobileImages = [
+    '1.gif',
+    '2.gif',
+    '3.gif',
+    '4.gif',
+    '5.gif',
+    '6.gif',
+    //'7.gif',
+    '8.gif',
+    '9.gif',
+    '10.gif',
+    //'11.gif',
+    '12.gif',
+    //'13.gif',
+    '14.gif',
+    //'15.gif',
+    //'16.gif',
+    '17.gif',
+    '18.gif',
+    '19.gif',
+    '20.gif',
+    '21.gif',
+    //'22.gif',
+    //'23.gif',
+    //'24.gif',
+    //'25.gif',
+    //'26.gif'
+  ];
+
+        var d = new Date(),
+      seconds = d.getSeconds();
+
+   var $cover = $('.cover'),
+  $coverImage = $cover.find('.js-bg');
+
+  //
+  // if it's a touch device, only load the mobile image array with the lighter images
+  if( $('html').hasClass('touch') ) {
+    $coverImage.attr('src', 'i/bg/' + mobileImages[getImageNumber(seconds)]);
+
+    $cover.imagesLoaded({
+      done: function() {
+        $('html').css({
+          'background': 'url(i/texture.png), url(i/bg/' + mobileImages[getImageNumber(seconds)] + ')',
+          'background-repeat': 'no-repeat, no-repeat',
+          'background-size': 'cover, cover'
+        });
+
+        $cover.removeClass('active');
+        showLogo();
+      }
+    });
+  } else {
+    $coverImage.attr('src', 'i/bg/' + images[getImageNumber(seconds)]);
+
+    $cover.imagesLoaded({
+      done: function() {
+        $('html').css({
+          'background': 'url(i/texture.png), url(i/bg/' + images[getImageNumber(seconds)] + ')',
+          'background-repeat': 'no-repeat, no-repeat',
+          'background-size': 'cover, cover'
+        });
+
+        $cover.removeClass('active');
+        showLogo();
+      }
+    });
+  }
+
+  function getImageNumber(secs) {
+
+    if(secs < images.length) {
+      return secs;
+    } else {
+      return getImageNumber(secs - images.length);
+    }
+
+  }
+
   //
   //
   // toggle the main header location change (active state)
@@ -104,24 +212,29 @@ $(function() {
   //
   // animate the nav items in
   function showNav() {
-    $('.main-nav li a').each(function(i) {
+    $('.main-nav li').each(function(i) {
       var $self = $(this);
 
       (function(i) {
         $self.addClass('animated  flipInX');
-      }).delay(i * 160);
+      }).delay(i * 120);
     });
   }
 
   //
   //
   // animate the logo in
-  (function() {
-    $('.logo').addClass('animated  flipInX');
+  function showLogo() {
+    var $logo = $('.js-logo');
+
     (function() {
+      $logo.addClass('animated  flipInX');
+    }).delay(400);
+    (function() {
+      $logo.removeClass('initial');
       showNav();
     }).delay(600);
-  }).delay(600);
+  }
 
 
 
